eb2be13ed49bb85ad3e4511b8feb53445901cc47
Svetlana Rashchupkina <svetlana.rashchupkina@gmail.com>
2020-04-06 15:13:19 +0200

    :bug: Fix env var

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
-// require('dotenv').config();
 
-const API_URL = process.env.API_URL || "http://localhost:3000";
+const API_URL = "https://zero-waste-chatbot.herokuapp.com";
 
 async function getId() {
   const sessionId = await axios.get(`${API_URL}/api/session`);
@@ -12,9 +11,9 @@ async function askAssistant(msg, sessionId) {
   const message = await axios.post(`${API_URL}/api/message`, {
     session_id: sessionId,
     input: {
-      message_type: "text",
-      text: msg,
-    },
+      mesage_type: "text",
+      text: msg
+    }
   });
   return message.data.result;
 }
